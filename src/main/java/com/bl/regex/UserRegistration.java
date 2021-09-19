99ea6915fca15dc5e3f38e261b94a0e4b38d666c
jumdemonali <jumdemonali@gmail.com>
2021-09-19 12:06:53 +0530

    Added: Checking email samples provided seperatly using regex

--- src/main/java/com/bl/regex/UserRegistration.java
+++ src/main/java/com/bl/regex/UserRegistration.java
@@ -4,14 +4,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static boolean password(String password) {
-        String regex = "^(?=.*[0-9])[0-9a-zA-Z!,@#$&*().]{8,}$";
+    public static boolean emailSamples(String email) {
+        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(password);
+        Matcher matcher = pattern.matcher(email);
         if (matcher.matches()) {
-            System.out.println("\nYour password " + password + " is in correct format.");
+            System.out.println("\nGiven email format " + email + " is in correct format.");
         } else {
-            System.out.println("\nYour password is not in correct format.");
+            System.out.println("\nGiven email format is not in correct format.");
         }
         return matcher.matches();
     }
--- src/test/java/com/bl/regex/UserValidationTest.java
+++ src/test/java/com/bl/regex/UserValidationTest.java
@@ -8,32 +8,58 @@ public class UserValidationTest {
     UserRegistration userRegistration = new UserRegistration();
 
     @Test
-    public void whenGivenPasswordRule3_WhenInProperFormat_ShouldReturnTrue() {
-
-        boolean validationStatus = userRegistration.password("monaliJumde1");
-        Assert.assertEquals(true, validationStatus);
+    public void WhenGivenEmail_IsInProperFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.emailSamples("abc.100@abc.com.au");
+        Assert.assertEquals(true, result);
     }
 
     @Test
-    public void whenGivenPasswordRule3_WhenNotInProperFormat_ShouldReturnFalse() {
-
-        boolean validationStatus = userRegistration.password("monaliJumde");
-        Assert.assertEquals(false, validationStatus);
+    public void WhenGivenEmail_IsNotInProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailSamples("abc()*@gmail.com");
+        Assert.assertEquals(false, result);
     }
 
     @Test
-    public void whenGivenPasswordRule4_WhenInProperFormat_ShouldReturnTrue() {
-
-        boolean validationStatus = userRegistration.password("monaliJumde@1");
-        Assert.assertEquals(true, validationStatus);
+    public void givenEmail2_WhenProper_ShouldReturnTrue() {
+        boolean result = userRegistration.emailSamples("abc.100@abc.com.au");
+        Assert.assertEquals(true, result);
     }
 
     @Test
-    public void whenGivenPasswordRule4_WhenNotInProperFormat_ShouldReturnFalse() {
-
-        boolean validationStatus = userRegistration.password("monaliJumde");
-        Assert.assertEquals(false, validationStatus);
+    public void givenEmail3_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailSamples("abc()*@gmail.com");
+        Assert.assertEquals(false, result);
     }
-
+    @Test
+    public void givenEmail4_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailSamples("abc123@gmail.a –");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail5_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailSamples("abc123@.com");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail6_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailSamples(".abc@abc.com");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail7_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailSamples("abc.@gmail.com –");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail8_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailSamples("abc@abc@gmail.com");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail9_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailSamples("abc..2002@gmail.com");
+        Assert.assertEquals(false, result);
+    }
 
 }
+
