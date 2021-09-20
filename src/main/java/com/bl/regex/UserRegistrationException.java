c507980bd091793009114d1a4e526c7f9545f47a
jumdemonali <jumdemonali@gmail.com>
2021-09-20 21:15:59 +0530

    Added: Lambda function to validate user entry

--- src/main/java/com/bl/regex/UserRegistrationException.java
+++
@@ -1,9 +0,0 @@
-package com.bl.regex;
-
-public class UserRegistrationException extends Exception{
-    public UserRegistrationException() {
-    }
-    public UserRegistrationException(String message){
-        super(message);
-    }
-}
